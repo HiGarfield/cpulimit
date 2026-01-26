547def3f1f0cf9ad936d8dc502ffaa59d6a3220e
HiGarfield <HiGarfield@126.com>
2026-01-27 00:15:52 +0800

    Improve multi_process_busy

--- tests/multi_process_busy.c
+++ tests/multi_process_busy.c
@@ -19,22 +19,49 @@
  * <https://www.gnu.org/licenses/>.
  */
 
+#include "../src/util.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 /**
  * @brief CPU load generator using fork
- * @return Always returns 0
- * @note This program creates 4 processes (original + 3 children) that
- *       each run an infinite busy loop, consuming CPU cycles for
- *       testing purposes.
+ * @param argc Command line argument count
+ * @param argv Command line arguments (optional: number of processes)
+ * @return Always returns 0 (unreachable)
+ * @note This program creates a specified number of processes (default: CPU
+ *       core count) that each run an infinite busy loop, consuming CPU cycles
+ *       for testing purposes. If a command line argument is provided, it
+ *       specifies the number of processes to create. Otherwise, the number of
+ *       CPU cores is used.
  */
-int main(void) {
-    /* Create two child processes, resulting in 4 total processes */
-    fork();
-    fork();
-    while (1) {
-        /* Infinite busy loop to consume CPU cycles */
-        ;
+int main(int argc, const char *argv[]) {
+    int i, num_procs;
+    num_procs = argc == 2 ? atoi(argv[1]) : get_ncpu();
+    num_procs = MAX(num_procs, 1);
+
+    /* Create num_procs-1 child processes (total num_procs processes) */
+    for (i = 1; i < num_procs; i++) {
+        pid_t pid = fork();
+        if (pid < 0) { /* fork failed */
+            fprintf(stderr, "fork failed\n");
+            kill(0, SIGKILL); /* Kill all created children */
+            exit(EXIT_FAILURE);
+        } else if (pid > 0) { /* Parent process */
+            /* Continue creating more children */
+        } else { /* Child process (pid == 0) */
+            /* Child should not create more processes */
+            break;
+        }
     }
+
+    /* All processes (parent and 3 children) enter infinite loop */
+    while (1) {
+        ; /* Busy loop consuming CPU */
+    }
+
+    /* Unreachable code */
     return 0;
 }
